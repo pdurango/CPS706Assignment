2fd105ae139d76fdf92bd7b201381fc02b6dd918
pdurango <silvestri_21@hotmail.com>
2018-04-10 16:05:50 -0400

    HerCDN sends only text-based files

--- Project/src/Client.java
+++ Project/src/Client.java
@@ -4,8 +4,10 @@ import java.util.LinkedList;
 
 public class Client
 {
-    public static final int PORT = 40290;
-    public static final String IPADDRESS = "localhost"; //99.246.236.65
+    public static final int PORTHIS = 40290;
+    public static final String IPADDRESSHIS = "localhost"; //99.246.236.65
+    public static final int PORTHER = 40291;
+    public static final String IPADDRESSHER = "localhost"; //99.246.236.65
 
     public static void main(String[] args) throws Exception {
         new Client().runClient();
@@ -33,6 +35,7 @@ public class Client
                     if (links.get(i).contains(clientChoice))
                     {
                         System.out.println(links.get(i));
+                        getCDNFileTCP(links.get(i));
                     }
                 }
             }
@@ -42,7 +45,7 @@ public class Client
     }
 
 
-    public LinkedList<String> getIndexTCP() throws IOException
+    public LinkedList<String> getIndexTCP() throws IOException //HisCinemaServer
     {
         InputStream inputStream;
         OutputStream outputStream;
@@ -52,14 +55,14 @@ public class Client
         int count;
         File file = new File("src/ClientFiles/HisCinemaIndex.html");
         LinkedList<String> linksFromIndex;
-        String serverHTTPMessage = "";
+        String serverHTTPMessage;
 
         //clientSocket = new Socket("localhost", PORT);
-        clientSocket = new Socket(InetAddress.getByName(IPADDRESS), PORT);
+        clientSocket = new Socket(InetAddress.getByName(IPADDRESSHIS), PORTHIS);
 
         outToServer = new PrintWriter(clientSocket.getOutputStream(), true); //outputs to server
 
-        outToServer.println("GET index.html /1.1");
+        outToServer.println("GET index.html HTTP/1.1");
         outToServer.flush();
 
         bytes = new byte[1024 * 2];
@@ -70,9 +73,53 @@ public class Client
         }
 
         serverHTTPMessage = htmlParserHTTPMessage(file);
-        System.out.println("message: " + serverHTTPMessage);
+        System.out.println("message from HisCinemaServer: " + serverHTTPMessage);
         linksFromIndex = htmlParser(file);
         return linksFromIndex;
+    }
+
+
+    public void getCDNFileTCP(String videoLink) throws IOException //HerCDNServer
+    {
+        InputStream inputStream;
+        OutputStream outputStream;
+        byte[] bytes;
+        Socket clientSocket;
+        PrintWriter outToServer;
+        int count;
+        File file = new File("src/ClientFiles/HerCDNIndex.html");
+        String serverHTTPMessage;
+        String CDNServerName = "www.herCDN.com/F";
+
+        clientSocket = new Socket(InetAddress.getByName(IPADDRESSHER), PORTHER);
+        outToServer = new PrintWriter(clientSocket.getOutputStream(), true); //outputs to server
+
+
+        String outHTTPRequest = "GET " + CDNServerName + Integer.parseInt(videoLink.replaceAll("[\\D]", "")) + " HTTP/1.1";
+        System.out.println("outHTTPRequest: " + outHTTPRequest);
+
+        outToServer.println(outHTTPRequest);
+        outToServer.flush();
+
+        /*
+        bytes = new byte[1024 * 16];
+
+        while ((count = inputStream.read(bytes)) >= 0) {
+            outputStream.write(bytes, 0, count);
+        }
+        */
+
+        inputStream = clientSocket.getInputStream();
+        outputStream = new FileOutputStream(file);
+        long length = file.length();
+        byte[] bytes1 = new byte[16 * 1024];
+        int count1;
+        while ((count1 = inputStream.read(bytes1)) != -1) {
+            outputStream.write(bytes1, 0, count1);
+        }
+
+        serverHTTPMessage = htmlParserHTTPMessage(file, Integer.parseInt(videoLink.replaceAll("[\\D]", "")));
+        System.out.println("message from HerCDNServer: " + serverHTTPMessage);
     }
 
 
@@ -90,9 +137,11 @@ public class Client
         return links;
     }
 
+    @SuppressWarnings("Duplicates")
     public String htmlParserHTTPMessage (File htmlFile) throws IOException
     {
         String message = "";
+        File file = new File("src/ClientFiles/HerCDNIndex.html");
         try (BufferedReader br = new BufferedReader(new FileReader(htmlFile))) {
             String line;
             while ((line = br.readLine()) != null) {
@@ -106,10 +155,67 @@ public class Client
                     message = "505 Version Not Supported HTTP/1.1";
                     break;
                 }
+                else if (line.contains("400 BAD REQUEST HTTP/1.1"))
+                {
+                    message = "400 BAD REQUEST HTTP/1.1";
+                    break;
+                }
                 else if (line.contains("404 File Not Found HTTP/1.1"))
                 {
                     message = "404 File Not Found HTTP/1.1";
                     break;
+                }
+            }
+        }
+        return message;
+    }
+
+    @SuppressWarnings("Duplicates")
+    public String htmlParserHTTPMessage (File htmlFile, int fileNum) throws IOException
+    {
+        String message = "";
+        String filePath = "src/ClientFiles/" + fileNum + ".txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(htmlFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.contains("200 OK HTTP/1.1"))
+                {
+                    message = "200 OK HTTP/1.1";
+                    break;
+                }
+                else if (line.contains("505 Version Not Supported HTTP/1.1"))
+                {
+                    message = "505 Version Not Supported HTTP/1.1";
+                    break;
+                }
+                else if (line.contains("400 BAD REQUEST HTTP/1.1"))
+                {
+                    message = "400 BAD REQUEST HTTP/1.1";
+                    break;
+                }
+                else if (line.contains("404 File Not Found HTTP/1.1"))
+                {
+                    message = "404 File Not Found HTTP/1.1";
+                    break;
+                }
+                else
+                {
+                    BufferedReader inputStream = new BufferedReader(new FileReader (htmlFile));
+                    File UIFile = new File(filePath);
+                    // if File doesnt exists, then create it
+                    if (!UIFile.exists()) {
+                        UIFile.createNewFile();
+                    }
+                    FileWriter filewriter = new FileWriter(UIFile.getAbsoluteFile());
+                    BufferedWriter outputStream= new BufferedWriter(filewriter);
+                    String count;
+                    while ((count = inputStream.readLine()) != null) {
+                        outputStream.write(count);
+                    }
+                    outputStream.flush();
+                    outputStream.close();
+                    inputStream.close();
                 }
             }
         }
--- Project/src/HerCDNServer.java
+++ Project/src/HerCDNServer.java
@@ -1,39 +1,43 @@
 import java.io.*;
 import java.net.*;
 
-public class HisCinemaServer
+public class HerCDNServer
 {
-    static int PORT = 40290;
+    static int PORT = 40291;
     private Socket connectionSocket;
 
-    public static void main(String[] args) throws Exception{
-        new HisCinemaServer().runServer();
+    public static void main(String[] args) throws Exception
+    {
+        new HerCDNServer().runServer();
     }
 
     @SuppressWarnings("Duplicates")
-    public void runServer() throws Exception{
+    public void runServer() throws Exception
+    {
 
-        String path = "src/HisCinemaFiles/";
+        String path = "src/HerCDNFiles/";
         String messageReceive;
         String fileName;
 
         ServerSocket welcomeSocket = new ServerSocket(PORT);
         //welcomeSocket.setSoTimeout(60000);
 
-        System.out.println("HisCinemaServer up and ready...");
+        System.out.println("HerCDNServer up and ready...");
 
-        while(true){
+        while (true)
+        {
             connectionSocket = welcomeSocket.accept();
 
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream (connectionSocket.getOutputStream());
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
             messageReceive = inFromClient.readLine();
 
 
             if (messageReceive.split(" ").length == 3)
             {
                 String[] tokens = messageReceive.split(" ");
-                String getRequestFile = "src/HisCinemaFiles/" + tokens[1];
+                String getRequestFile = path + Integer.parseInt(tokens[1].replaceAll("[\\D]", "")) + ".txt";
+                System.out.println("getRequestFile: " + getRequestFile);
                 File fileCheck = new File(getRequestFile);
 
                 if (!tokens[0].equals("GET"))
@@ -50,11 +54,10 @@ public class HisCinemaServer
                     outToClient.close();
                 } else
                 {
-                    outToClient.writeBytes("200 OK HTTP/1.1");
-                    //outToClient.flush();
-                    fileName = this.parseRequest(messageReceive);
-                    System.out.println(fileName);
-                    File file = new File(path + fileName);
+                    //outToClient.writeBytes("200 OK HTTP/1.1"); //UNCOMMENT
+                    //outToClient.flush(); //UNCOMMENT
+
+                    File file = new File(getRequestFile);
                     sendFile(file);
                     outToClient.close();
                 }
@@ -65,27 +68,39 @@ public class HisCinemaServer
                 outToClient.close();
             }
         }
+
     }
 
-    public String parseRequest(String message){
+    public String parseRequest(String message)
+    {
         String[] tokens = message.split(" ");
         message = tokens[1].toString();
         return message;
     }
 
     @SuppressWarnings("Duplicates")
-    public void sendFile(File file) throws Exception{
+    public void sendFile(File file) throws Exception
+    {
         FileInputStream readFile = new FileInputStream(file);
         DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-        byte[] buffer = new byte[4096];
-        while (readFile.read(buffer) > 0) {
-            outToClient.write(buffer);
+        byte[] buffer = new byte[16 * 1024];
+        int len = 0;
+        while ((len = readFile.read(buffer)) != -1) {
+            outToClient.write(buffer, 0, len);
+            System.out.println("LEN: " + len);
         }
+
+        /*
+        byte[] buffer = new byte[1024 * 16];
+        while (readFile.read(buffer) > 0)
+        {
+            outToClient.write(buffer);
+        }*/
+
         outToClient.flush();
         readFile.close();
         outToClient.close();
         System.out.println("File sent successfully to: " + connectionSocket.getInetAddress().getHostName());
     }
-
 }
