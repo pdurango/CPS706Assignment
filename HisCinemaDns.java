3a95a2aa82428b610ab9fb9c4871d8f65086a7fb
Lir2303 <liran.fraiman@ryerson.ca>
2018-04-10 15:54:07 -0400

    HisCinemaDns.java with Threading

--- HisCinemaDns.java
+++ HisCinemaDns.java
@@ -6,14 +6,10 @@ public class HisCinemaDns {
 	private Socket connectionSocket;
 
 	public static void main(String[] args) throws Exception{
-		new HisCinemaDns().runServer();
+		new HisCinemaDns().runTCPServer();
 	}
 	
-	public void runServer() throws Exception{
-		String path = "C:/Users/LiranF/workspace/CPS706- W2018 Assignment/"; //Path where index file resides
-		String messageReceive; // Variable will store the 'GET' request received from client
-		String fileLocation; // Variable will store the directory and name of file. i.e. directory/nameoffile.html
-		
+	public void runTCPServer() throws Exception{
 		ServerSocket welcomeSocket = new ServerSocket(PORT); // Create socket with port  
 		welcomeSocket.setSoTimeout(60000); // Set timeout for server of 1 minute
 		
@@ -21,35 +17,56 @@ public class HisCinemaDns {
 		
 		while(true){
 			connectionSocket = welcomeSocket.accept(); // Listens to a connection and accepts it from client. Returns new socket
-			BufferedReader inFromClient = 
-					new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-			
-			messageReceive = inFromClient.readLine();
-			fileLocation = this.parseRequest(messageReceive);
-			File file = new File(path + fileLocation);
-			sendFile(file);
+			new ServerThread(connectionSocket).start();
 		}
 		
 	}
 	
-	public String parseRequest(String message){
-		String[] tokens = message.split(" ");
-		message = tokens[1].toString();
-		return message;
-	}
-	
-	public void sendFile(File file) throws Exception{
-		FileInputStream readFile = new FileInputStream(file); 
-		DataOutputStream outToClient =
-				new DataOutputStream(connectionSocket.getOutputStream());
+	public class ServerThread extends Thread{
+		Socket socket;
+		ServerThread(Socket socket){
+			this.socket = socket;
+		}
+		public void run(){
+			try{
+				String path = "C:/Users/LiranF/workspace/CPS706- W2018 Assignment/"; //Path where index file resides
+				String messageReceive; // Variable will store the 'GET' request received from client
+				String fileLocation; // Variable will store the directory and name of file. i.e. directory/nameoffile.html
+				
+				BufferedReader inFromClient = 
+						new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+				
+				messageReceive = inFromClient.readLine();
+				fileLocation = this.parseRequest(messageReceive);
+				File file = new File(path + fileLocation);
+				sendFile(file);
+			}catch (IOException e){
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		
-		byte[] buffer = new byte[4096];
-		while (readFile.read(buffer) > 0) {
-			outToClient.write(buffer);
-		}  
-		outToClient.flush();
-		readFile.close();
-		outToClient.close();
-		System.out.println("File sent succesfully!");
+		public String parseRequest(String message){
+			String[] tokens = message.split(" ");
+			message = tokens[1].toString();
+			return message;
+		}
+		
+		public void sendFile(File file) throws Exception{
+			FileInputStream readFile = new FileInputStream(file); 
+			DataOutputStream outToClient =
+					new DataOutputStream(connectionSocket.getOutputStream());
+			
+			byte[] buffer = new byte[4096];
+			while (readFile.read(buffer) > 0) {
+				outToClient.write(buffer);
+			}  
+			outToClient.flush();
+			readFile.close();
+			outToClient.close();
+			System.out.println("File sent succesfully!");
+		}
+	
 	}
 }
